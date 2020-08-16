d31937ea571d9b166ef6c568de138386f1fd9b09
roku3 <down01134@gmail.com>
2020-08-16 15:35:57 +0900

    足場が短い時の描画修正

--- Game/Game/src/Game.cpp
+++ Game/Game/src/Game.cpp
@@ -313,10 +313,10 @@ void Game::footUpdate() {
 void Game::footDrawBefore() const {
     for (int i = 0; i < FT_NUM; i++) {
         // 左右の壁の描画
-        if (!foots[i].isFrontL) {
+        if (!foots[i].isFrontL && foots[i].dirL > Math::HalfPi) {
             drawBox(foots[i].posRootXL, foots[i].drawPosY, foots[i].posXL, FT_HEIGHT).draw(Palette::Red);
         }
-        if (!foots[i].isFrontR) {
+        if (!foots[i].isFrontR && foots[i].dirR < Math::HalfPi) {
             drawBox(foots[i].posRootXR, foots[i].drawPosY, foots[i].posXR, FT_HEIGHT).draw(Palette::Blue);
         }
     }
@@ -325,10 +325,10 @@ void Game::footDrawBefore() const {
 void Game::footDraw() const {
     for (int i = 0; i < FT_NUM; i++) {
         // 左右の壁の描画
-        if (foots[i].isFrontL) {
+        if (foots[i].isFrontL && foots[i].dirL < Math::HalfPi * 3) {
             drawBox(foots[i].posRootXL, foots[i].drawPosY, foots[i].posXL, FT_HEIGHT).draw(Palette::Red);
         }
-        if (foots[i].isFrontR) {
+        if (foots[i].isFrontR && foots[i].dirR > Math::HalfPi * 3) {
             drawBox(foots[i].posRootXR, foots[i].drawPosY, foots[i].posXR, FT_HEIGHT).draw(Palette::Blue);
         }
         
